0cee8df9b2151794008290a69073c74f7033a19a
huazhou.whz <huazhou19@gmail.com>
2016-10-08 22:19:48 +0800

    add some code

--- WIFIADBIntelliJPlugin/src/main/java/adb/wifi/woaiwhz/window/RootWindowHolder.java
+++ WIFIADBIntelliJPlugin/src/main/java/adb/wifi/woaiwhz/window/RootWindowHolder.java
@@ -1,6 +1,7 @@
 package adb.wifi.woaiwhz.window;
 
-import adb.wifi.woaiwhz.base.*;
+import adb.wifi.woaiwhz.base.Config;
+import adb.wifi.woaiwhz.component.Notify;
 import adb.wifi.woaiwhz.listener.CustomInputVerifier;
 import adb.wifi.woaiwhz.listener.LaunchWebBrowser;
 import adb.wifi.woaiwhz.listener.NumberDocumentFilter;
@@ -9,11 +10,15 @@ import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
+import org.apache.http.util.TextUtils;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 /**
  * Created by huazhou.whz on 2016/10/7.
@@ -152,6 +157,77 @@ public class RootWindowHolder implements ToolWindowFactory,ActionListener{
     }
 
     private void handleConnection(){
-        // TODO: 2016/10/7
+        if(!verifyIpText() || !verifyPortText()){
+            Notify.error("Fail to verify ip address or port number");
+            return;
+        }
+
+        Notify.alert("success : " + gainIpAddressWithPortNumber());
+        cleanText();
+    }
+
+    private String gainIpAddressWithPortNumber(){
+        final StringBuilder builder = new StringBuilder();
+
+        for (JTextField field : mIPTexts){
+            builder.append(field.getText());
+            builder.append(".");
+        }
+
+        final int length = builder.length();
+        builder.replace(length - 1,length,":");
+        builder.append(mPort.getText());
+
+        return builder.toString();
+    }
+
+    private boolean verifyPortText(){
+        final String text = mPort.getText();
+
+        if(TextUtils.isBlank(text)){
+            return false;
+        }
+
+        try {
+            int port = Integer.valueOf(text);
+
+            if(port >= 0 && port < 1 << 16){
+                return true;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+
+        }
+
+        return false;
+    }
+
+    private void cleanText(){
+        for(JTextField field : mIPTexts){
+            field.setText("");
+        }
+        mPort.setText(String.valueOf(Config.DEFAULT_PORT));
+    }
+
+    private boolean verifyIpText(){
+        for (JTextField field : mIPTexts){
+            final String text = field.getText();
+
+            if(TextUtils.isBlank(text)){
+                return false;
+            }
+
+            try{
+                int ip = Integer.valueOf(text);
+                if(ip < 0 && ip >= 1 << 8){
+                    return false;
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+                return false;
+            }
+        }
+
+        return true;
     }
 }
